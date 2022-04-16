8bcf93ec19620288b2cdcafc3b384aed59009b73
kje031417 <102713751+kje031417@users.noreply.github.com>
2022-04-16 20:11:20 +0900

    ADD sql

---
+++ project_naw/src/main/webapp/sql/naw_inquiry.sql
@@ -0,0 +1,75 @@
+-- 1) 문의게시판 테이블
+create table inquiry (
+    inquiry_code number,	                        --문의글 번호
+	user_id varchar2(15) not null,	                --회원 id
+    item_code varchar2(100) not null,               --상품 번호
+	inquiry_subject varchar2(100) not null,	        --제목
+	inquiry_content varchar2(100) not null,	        --내용
+	re_ref number not null,	                        --관련 글 번호
+	re_lev number not null,	                        --답글 레벨
+	re_seq number not null,	                        --관련 글 중 출력순서
+	inquiry_readcount number not null,             	--조회수
+	inquiry_date date,        	                    --작성일
+    primary key(inquiry_code)
+);
+
+-- 2) 테이블 삭제
+drop table inquiry purge;
+
+-- 3) 테이블 확인
+select * from tab;
+
+-- 4) 시퀀스 객체 생성
+create sequence inquiry_code nocycle nocache;
+
+-- 5) 시퀀스 객체 삭제
+drop sequence inquiry_code;
+
+-- 6) 데이터 저장하기
+insert into inquiry 
+values(inquiry_code.nextval, 'hong1234', '002',  'test', 'content', 
+inquiry_code.currval, 0, 0, 0, sysdate);    -- currval : 현재 시퀀스값 읽어오기
+
+-- 7) 전체 내용 확인
+select * from inquiry order by inquiry_code desc;
+
+-- 8) 상세 데이터 1줄 뽑아오기
+select * from inquiry where inquiry_code=1;
+
+-- 9) 데이터 삭제
+delete inquiry where inquiry_code=3;
+
+-- 10) db 저장
+commit;
+
+-- 11) 조회수 증가
+update inquiry set inquiry_readcount = inquiry_readcount + 1 where inquiry_code=1;
+
+-- 12) 글쓴이인지 확인 : session id 와 db에 등록된 게시물 작성자 id가 일치
+select * from inquiry where inquiry_code=45;
+
+-- 13) 글 수정
+update inquiry set inquiry_subject='test1', inquiry_content='content1'
+where inquiry_code=1;
+
+-- 14) 답글 저장
+---- 1.기존글의 re_seq값 정리 : 원글보다 큰 re_seq값을 1씩 증가
+update inquiry set re_seq = re_seq + 1
+where re_ref=0 and re_seq>0 and item_code='001';
+
+---- 2.답글 저장
+---- re_ref = 원글의 re_ref
+---- re_lev = 원글의 re_lev + 1
+---- re_seq = 원글의 re_seq + 1
+insert into inquiry values
+(seq_num.nextval, '답글작성자id', '상품번호', '제목', '내용', 1, 1, 1, 0, sysdate);
+
+-- 16) 답글에 대한 인덱스 뷰
+---- => re_ref 기준 내림차순, 같은 값들은 re_seq 기준 오름차순으로 정렬시킴
+select * from
+(select rownum rn, tt.* from
+(select * from inquiry order by re_ref desc, re_seq asc) tt)
+where rn>=1 and rn<=5 and item_code='002';
+
+-- 17) 특정 상품의 문의글 개수 구하기
+select count(*) as cnt from inquiry where item_code='002';
---
+++ project_naw/src/main/webapp/sql/naw_review.sql
@@ -0,0 +1,56 @@
+-- 1) 리뷰게시판 테이블
+create table review(
+    review_code number,	                    --리뷰글 번호
+	user_id varchar2(15) not null,	        --회원 id
+    item_code varchar2(100) not null,       --상품 번호
+	review_content varchar2(100) not null,	--내용
+	review_star number,	                    --별점
+	review_date date,	                    --작성일
+    primary key(review_code)
+);
+
+-- 2) 테이블 삭제
+drop table review purge;
+
+-- 3) 테이블 확인
+select * from tab;
+
+-- 4) 시퀀스 객체 생성
+create sequence review_code nocycle nocache;
+
+-- 5) 시퀀스 객체 삭제
+drop sequence review_code;
+
+-- 6) 데이터 저장하기
+insert into review 
+values(review_code.nextval, 'hong1234', '002', 'content', 5, sysdate);
+
+-- 7) 전체 내용 확인
+select * from review order by review_code desc;
+
+-- 8) 특정 상품 하나의 리뷰글 모아보기
+select * from review where item_code=002;
+
+-- 9) 데이터 수정(보류)
+update review 
+set review_content='content1', review_star=3 
+where review_code=1;
+
+-- 10) 데이터 삭제
+delete review where review_code=8;
+
+-- 11) db 저장
+commit;
+
+-- 12) 인덱스 뷰
+-- hidden column => rownum
+select * from
+(select rownum rn, tt.* from
+(select * from review order by review_code desc) tt)     --내림차순 정렬
+where rn>=1 and rn<=5;
+
+-- 13) 전체 데이터 개수 구하기
+select count(*) as cnt from review;
+
+-- 14) 상품당 리뷰글 수 뽑아오기
+select count(*) as cnt from review;
